0d8889f1879f01f34aff1301391792620f621629
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-01-30 09:53:28 -0600

    working on react contructors

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-        return <div>Latitude</div>;
-};
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
     render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
         return <div>Latitude: </div>
     }
 
