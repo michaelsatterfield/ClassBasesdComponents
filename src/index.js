83badd1e095faf9a4d11638375640f8b612c0084
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-02-01 10:49:57 -0600

    learning contstructor in react

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
     constructor(props) {
         super(props);
-    }
 
+        this.state = {lat: null };
+    }
 
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
-        return <div>Latitude: </div>
+        return <div>Latitude: {this.state.lat}</div>
     }
 
 }
